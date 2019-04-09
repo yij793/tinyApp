f5f6cd8b4c2f0206d6ec761a4cef15cf95cffdfe
YIYAO <jianyy1990@hotmail.com>
2019-04-09 22:46:33 +0000

    redirect complete

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var app = express();
+var express = require("express");// adding express into our servers
+var app = express();/// store express() in a var
 var PORT = 8081; // default port 8081
+const bodyParser = require("body-parser"); ///add bodyParser into our servers
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
+// port listener
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
@@ -24,9 +26,39 @@ app.get('/urls', (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars)
 })
-app.get("/urls/:shortURL", (req, res) => {
+/*app.get("/urls/:shortURL", (req, res) => {
     let values = req.params.shortURL;
     let data = urlDatabase[values]
     let templateVars = { shortURL: values, longURL: data };
     res.render("urls_show", templateVars);
 });
+*/
+app.get('/urls/new', (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_new", templateVars);
+})
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+    urlDatabase[generateRandomString()] = req.body.longURL
+    console.log(urlDatabase)
+});
+app.get("/u/:shortURL", (req, res) => {
+    // const longURL = ...
+    let values = req.params.shortURL;
+    let data = urlDatabase[values]
+    let templateVars = { shortURL: values, longURL: data };
+    res.redirect(templateVars.longURL);
+});
+
+function generateRandomString() {
+    var randomCode = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const possibleSize = possible.length
+    for (i = 0; i < 6; i++) {
+        randomCode += possible[Math.floor(Math.random() * possibleSize)]
+    }
+    return randomCode
+}
