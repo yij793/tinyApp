4034453045090e3f908cb4240b6f6c863f825788
YIYAO <jianyy1990@hotmail.com>
2019-04-09 17:34:36 +0000

    import HTML

--- express_server.js
+++ express_server.js
@@ -10,7 +10,12 @@ var urlDatabase = {
 app.get("/", (req, res) => {
     res.send("Hello!");
 });
-
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+});
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
+});
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
