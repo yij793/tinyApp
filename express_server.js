05fd6304005e80895219facc8876555e90c680b5
YIYAO <jianyy1990@hotmail.com>
2019-04-11 19:40:33 +0000

    now we can register and login

--- express_server.js
+++ express_server.js
@@ -11,6 +11,18 @@ var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
+const users = {
+    "userRandomID": {
+        id: "userRandomID",
+        email: "user@example.com",
+        password: "purple-monkey-dinosaur"
+    },
+    "user2RandomID": {
+        id: "user2RandomID",
+        email: "user2@example.com",
+        password: "dishwasher-funk"
+    }
+}
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -27,8 +39,10 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get('/urls', (req, res) => {
-    let templateVars = { urls: urlDatabase, username: req.cookies.username };
-
+    let templateVars = { urls: urlDatabase, user_id: req.cookies.user_id };
+    console.log(req.cookies.user_id)
+    console.log(users)
+    console.log(Object.keys(users).length)
     res.render("urls_index", templateVars)
 })
 
@@ -46,7 +60,6 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/urls", (req, res) => {
-    console.log(req, res);  // Log the POST request body to the console
     res.send("Ok");         // Respond with 'Ok' (we will replace this)
     urlDatabase[generateRandomString()] = req.body.longURL
     console.log(urlDatabase)
@@ -74,11 +87,85 @@ function generateRandomString() {
     }
     return randomCode
 }
+function checkRegister(em) {
+    const userID = Object.keys(users)
+    console.log('userID is:   ', userID)
+    const user = userID.filter(item => {
+        return users[item].email === em
+    })
+    if (user > 0) {
+        return false
+    } else { return true }
+}
+function loginCheck(em, pw) {
+    const userID = Object.keys(users)
+    console.log('userID is:   ', userID)
+    const user = userID.filter(item => {
+        console.log('items are:  ', users[item].email)
+        console.log('password input is:   ', pw)
+        console.log('email input is:   ', typeof em)
+        console.log('email inside users is :  ', typeof users[item].email)
+        console.log('true or false?   ', users[item].email === em)
+        return users[item].email === em
+    })
+    console.log('user name is:   ', user)
+    if (user.length > 0) {
+        console.log(users[user].password)
+        if (users[user].password === pw) {
+            return true
+        }
+    }
+
+
+}
 app.post('/login', (req, res) => {
-    res.cookie('username', req.body.username)
-    res.redirect('/urls')
+    const { email, password } = req.body
+    console.log('input password is   ', password)
+    console.log('input email is   ', email)
+    //see if email is in data and if it match the password
+    if (loginCheck(email, password)) {
+        res.cookie('user_id', email)
+        res.redirect('/urls')
+    } else {
+        res.status(404)        // HTTP status 404: NotFound
+            .send('user not found');
+    }
+
+})
+app.get('/login', (req, res) => {
+
+    res.render('urls_login')
 })
 app.post('/logout', (req, res) => {
-    res.clearCookie('username')
+    res.clearCookie('user_id')
     res.redirect('/urls')
+})
+app.get('/register', (req, res) => {
+    const templateVars = ''
+    res.render('urls_register', templateVars)
+
+})
+app.post('/register', (req, res) => {
+    res.cookie('user_id', req.body.email)
+    //// ID generator to give and ID of 'object size +1'
+    const id = `user${Object.keys(users).length + 1}randomID`;
+    const { email, password } = req.body
+    users[id] = {
+        id: id,
+        email: email,
+        password: password
+    }
+    if (checkRegister(email)) {
+        res.cookie('user_id', email)
+        res.redirect('/urls')
+    } else {
+        res.status(404)        // HTTP status 404: NotFound
+            .send('this email has already been register');
+    }
+
+
+
+
+
+
 })
